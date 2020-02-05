69f8ff7b8cc4a776deff195fd2ea65eee5bd059d
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-02-05 18:04:24 +0000

    read file

---
+++ tolvuverkefni2.R
@@ -0,0 +1,6 @@
+library(tidyverse)library(readr)
+library(dplyr)
+library(lubridate) # pakki med follum fyrir vinnslu med dagsetningar (date eda datetime breytur)
+library(ggplot2)
+
+oo = read_csv("husnaedi.csv")
