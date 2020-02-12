ea28bf6cd328ece3b136e9a64450de9fa5cbb6fc
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-02-12 19:09:58 +0000

    b c

--- tolvuverkefni2.R
+++ tolvuverkefni2.R
@@ -9,7 +9,38 @@ oo = oo %>% rename("curent_value" = "nuvirdi",
                    "type" = "teg_eign",
                    "area" = "matssvaedi",
                    "size" = "ibm2",)
-
-
-oo <- oo%>%filter(area==90)
+## a)
+set.seed(0601)
+hverfi<-sample(c(20,90,100),1)
+oo <- oo%>%filter(area==hverfi)
 oo <- subset(oo, select = -c(area))
+oo <- filter(oo, type =="Íbúðareign")
+remove(hverfi)
+
+## b)
+# mean(oo$current_value)
+# min(oo$current_value)
+# max(oo$current_value)
+
+ggplot(oo, aes( x=curent_value)) + 
+  geom_histogram()+xlab("price")
+price_mean = mean(oo$curent_value)
+
+## c)
+set.seed(0601)
+someVector <- sapply((1:5000),
+  function(x) {mean(sample(oo$curent_value, x,replace = TRUE))})      
+
+qplot(x = 1:5000,
+      y = someVector,
+      geom="line")+ xlab("sample size")+ ylab("mean price") +
+      geom_hline(yintercept = price_mean, col="red")
+
+
+qplot(x = 1:5000,
+      y = sapply((1:5000), function(x)
+      {mean(sample(oo$curent_value, x,replace = TRUE))}),
+      geom="line")+ xlab("sample size")+ ylab("mean price") +
+      geom_hline(yintercept = price_mean, col="red")
+
+
