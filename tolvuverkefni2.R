69dff0c770faa7335b799afaecc85e05415d8d38
Oren93 <54531718+Oren93@users.noreply.github.com>
2020-02-15 13:35:53 +0000

    d

--- tolvuverkefni2.R
+++ tolvuverkefni2.R
@@ -15,6 +15,7 @@ hverfi<-sample(c(20,90,100),1)
 oo <- oo%>%filter(area==hverfi)
 oo <- subset(oo, select = -c(area))
 oo <- filter(oo, type =="Íbúðareign")
+oo <- subset(oo, select = -c(type))
 remove(hverfi)
 
 ## b)
@@ -43,4 +44,19 @@ qplot(x = 1:5000,
       geom="line")+ xlab("sample size")+ ylab("mean price") +
       geom_hline(yintercept = price_mean, col="red")
 
+## d)
+staerd <- c(2,5,20,400)
+staerd1 <- replicate(n = 10000, mean(sample(oo$curent_value,
+        staerd[1],replace = TRUE)),simplify = TRUE )
+staerd2 <- replicate(n = 10000, mean(sample(oo$curent_value,
+        staerd[2],replace = TRUE)),simplify = TRUE )
+staerd3 <- replicate(n = 10000, mean(sample(oo$curent_value,
+        staerd[3],replace = TRUE)),simplify = TRUE )
+staerd4 <- replicate(n = 10000, mean(sample(oo$curent_value,
+        staerd[4],replace = TRUE)),simplify = TRUE )
+remove(staerd)
 
+## e)
+staerd <- tibble(staerd1=staerd1, staerd2 = staerd2,
+                            staerd3 = staerd3,staerd4=staerd4)
+remove(staerd1,staerd2,staerd3,staerd4)
